1e066b7080aff1d69718613cc10bab9bf74e0694
dunleavyjack <dunleavyjack@gmail.com>
2021-04-11 23:47:30 +0900

    Fixed

--- src/pages/BrowsePage.js
+++ src/pages/BrowsePage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 
 const BrowsePage = () => {
-    return <Header text={'Browse'} />;
+    return <Header text={'Browse finds'} />;
 };
 
 export default BrowsePage;
